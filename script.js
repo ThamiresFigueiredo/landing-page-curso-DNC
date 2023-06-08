2c3188696cc45b35d2393dbc746744e11383a067
ThamiresFigueiredo <thamiresfcdejesus@gmail.com>
2023-06-08 12:31:09 -0300

    Versão Inicial

---
+++ script.js
@@ -0,0 +1,19 @@
+var Bruna = window.document.getElementById("Bruna")
+var Leonardo = window.document.getElementById("Leonardo")
+var Samantha = window.document.getElementById("Samantha")
+var setaDireita = window.document.getElementById("seta-direita")
+var setaEsquerda = window.document.getElementById("seta-esquerda")
+
+function RolarParaDireita() {
+    Bruna.style = "display:none"
+    Samantha.style = "display:flex"
+    setaDireita.style = "display:none"
+    setaEsquerda.style = "display:flex"
+}
+
+function RolarParaEsquerda() {
+    Bruna.style = "display:flex"
+    Samantha.style = "display:none"
+    setaDireita.style = "display:flex"
+    setaEsquerda.style = "display:none"
+}
